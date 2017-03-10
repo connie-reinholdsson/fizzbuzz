4813e7d63767c32cf2b6d208d3ee7ac2b6d43ab6
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-03-10 16:22:24 +0000

    Updated with fizzbuzz test, which failed, then corrected it in fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number % 3 == 0 && number % 5 == 0
+    return 'fizzbuzz'
+  elsif number % 3 == 0
     return 'fizz'
   else number % 5 == 0
     return 'buzz'
